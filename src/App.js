3541776d65d4269d601cc1a017834b66e8cab8c9
oleksandra-maslennikova <oleksandra.maslennikova@itomy.ch>
2024-01-21 16:58:13 +0100

    Set up websocket and push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import notification from "./notification.svg";
+import "./App.css";
+
+const publicKey =
+  "BCRMXt1KpONBNVLwaae1F5M6rRqBB-c79xXZ_UcWufu4ZHHAWtSJuStvUSx0wl8a110vQ4HU2aZnjXuoJumuGJs";
+
+// Function to convert a base64 string to a Uint8Array
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+
+  const rawData = atob(base64);
+  const buffer = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    buffer[i] = rawData.charCodeAt(i);
+  }
+
+  return buffer;
+}
 
 function App() {
+  const [socket, setSocket] = useState(null);
+  const [notificationsList, setNotificationsList] = useState([]);
+  const [openedNotifications, setOpenedNotifications] = useState([]);
+
+  useEffect(() => {
+    const ws = new WebSocket("wss:notification-server-gpxp.onrender.com");
+    setSocket(ws);
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!socket) return;
+
+    socket.onmessage = (event) => {
+      console.log({ event });
+      setNotificationsList((list) => [...list, event?.data]);
+
+      // Handle notification data here
+    };
+
+    if ("serviceWorker" in navigator && "PushManager" in window) {
+      console.log("pushManager", navigator.serviceWorker);
+      navigator.serviceWorker.ready.then((registration) => {
+        console.log({ registration });
+        registration.pushManager
+          .subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: urlBase64ToUint8Array(publicKey),
+          })
+          .then((subscription) => {
+            console.log("subscription");
+            // Send the subscription to the server
+            fetch("https://notification-server-gpxp.onrender.com/subscribe", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(subscription),
+            });
+          })
+          .catch((error) => {
+            console.error("Error subscribing to push notifications:", error);
+          });
+      });
+    }
+
+    return () => {
+      socket.onmessage = null;
+      // Cleanup WebSocket event listeners
+    };
+  }, [socket]);
+
+  const handleOpenNotifications = () => {
+    setOpenedNotifications([...notificationsList]);
+    setNotificationsList([]);
+    setTimeout(() => {
+      setOpenedNotifications([]);
+    }, 3000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
+      <header className="header">
         <a
-          className="App-link"
+          className="link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img src={logo} className="logo" alt="logo" />
         </a>
+        <div className="usernameWrapper">
+          <span>Hi, Sasha</span>
+          <div className="notificationWrapper">
+            <img
+              src={notification}
+              className="notificationIcon"
+              alt="notification"
+            />
+            {!!notificationsList.length && (
+              <div
+                className="notificationCount"
+                onClick={handleOpenNotifications}
+              >
+                {notificationsList.length}
+              </div>
+            )}
+          </div>
+          {!!openedNotifications.length && (
+            <div className="openedNotifications">
+              {openedNotifications.map((item, index) => {
+                return <div key={index}>{item}</div>;
+              })}
+            </div>
+          )}
+        </div>
       </header>
     </div>
   );
